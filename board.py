77eefc6a4edbf88cdd417c221af4ca58c13b75b5
Kevin Nguyen <nguyenkt6123@gmail.com>
2020-04-15 18:41:27 -0400

    migrated to pygame, changed board color and background, and added a King to a sqaure

--- board.py
+++ board.py
@@ -2,11 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as tk
 import sound
+import pygame
+from PIL import Image
 
-chessBoard = np.zeros((8,8))
-chessBoard[1::2, 0::2] = 1
-chessBoard[0::2, 1::2] = 1
+# initialize pygame
+pygame.init()
 
-plt.imshow(chessBoard, cmap='binary')
+# set window size 
+gameDisplay = pygame.display.set_mode((800, 800))
+
+# window name
+pygame.display.set_caption('Nickelodeon Chess')
+gameExit = False
+
+# orange background, we'll change as needed
+gameDisplay.fill((51, 153, 255))
+
+squareSize = 80 
+count = 0
+
+image = pygame.image.load('./images/king.png')
+
+# loop to print out board, 64 squares
+for i in range(0, 8):
+    for j in range(0, 8):
+        if count % 2 == 0:
+            # print black square
+            pygame.draw.rect(gameDisplay, (204,102,0), [squareSize*j + 50, squareSize*i + 50, squareSize, squareSize])
+        else:
+            # print white squares
+            pygame.draw.rect(gameDisplay, (255,255,255), [squareSize*j + 50, squareSize*i + 50, squareSize, squareSize])
+        count += 1
+    # -1 to for alternating rows
+    count -= 1
+gameDisplay.blit(image, (50,50))
+
+pygame.display.update()
 sound.music()
-plt.show()
+
+while not gameExit:
+    for event in pygame.event.get():
+        print(event)
+
+pygame.quit()
+quit()
