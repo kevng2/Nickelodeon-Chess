2fb150b288199b92e4777f8999a7e2e192f4fd1d
Kevin Nguyen <nguyenkt6123@gmail.com>
2020-04-15 19:04:57 -0400

    added a list of music. Added random music functionality

--- sound.py
+++ sound.py
@@ -1,7 +1,13 @@
 from pygame import mixer
 import time
+import random
 
 def music():
+    musicList = ["spongebob.ogg", "i_hate_people.ogg", "krabs_counting_money.ogg"]
+
+    # for choosing a random song in the list
+    r = random .randint(0,2)
+
     mixer.init()
-    mixer.music.load("spongebob.ogg")
+    mixer.music.load("./sound/" + musicList[r])
     mixer.music.play()
