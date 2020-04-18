de47c36a7026f1b65f01060dc3fbe10ff3430903
Justin Williams <jw1116@live.com>
2020-04-18 17:19:19 -0400

    Update sound.py

--- sound.py
+++ sound.py
@@ -2,7 +2,7 @@ from pygame import mixer
 import time
 import random
 
-def music():
+'''def music():
     musicList=["spongebob.ogg", "i_hate_people.ogg", "krabs_counting_money.ogg", "all_that_theme_song.ogg", "danny_phantom.ogg", "drake_and_josh.ogg",
      "fairly_oddparents.ogg", "hey_arnold.ogg", "iCarly.ogg", "jimmy_neutron.ogg", "kenan_and_kel.ogg", "rugrats.ogg", "spongebob_theme.ogg",
      "teenage_robot.ogg", "victorious.ogg", "zoey101.ogg"]
@@ -24,7 +24,7 @@ def music():
 musicList = ["spongebob.ogg", "i_hate_people.ogg", "krabs_counting_money.ogg", "all_that_theme_song.ogg", "danny_phantom.ogg", "drake_and_josh.ogg",
  "fairly_oddparents.ogg", "hey_arnold.ogg", "iCarly.ogg", "jimmy_neutron.ogg", "kenan_and_kel.ogg", "rugrats.ogg", "spongebob_theme.ogg",
  "teenage_robot.ogg", "victorious.ogg", "zoey101.ogg"]
-currSong = None
+currSong = None'''
 
 def music():
     global currSong, musicList
