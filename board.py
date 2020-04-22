fdd1f6445a62b465405daa475663ed896b6e5740
jmaham23 <48227683+jmaham23@users.noreply.github.com>
2020-04-22 00:06:53 -0500

    Show Available Moves
    Added minimal support. Only works for bishop and Knight. I need to figure out how to keep the board positions updated

--- board.py
+++ board.py
@@ -8,6 +8,35 @@ def printBoardPieces(data):
     for piece in data:
         gameDisplay.blit(piece.image, (piece.rect))
 
+
+def draw_move(clicked, sprites):
+    move = possible_moves(clicked.x, clicked.y)
+    for n in move:
+        if n[1] < 0 or n[0] < 0 or n[1] > 7 or n[0] > 7:
+            continue
+        #elif (al for al in sprites if al.x and al.y in n[0] and n[1]):
+            #continue
+        else:
+            pygame.draw.circle(gameDisplay, (0, 205, 0), ((340+(n[1]*80)), (240+(n[0]*80))), 7)
+
+
+#function takes in a value for the piece coordinate and gives possible moves
+def possible_moves(x, y):
+    moves = [] #creating an empty list of tuples of coordinates for where a piece can move
+
+    if boardPosition.boardArray[x][y].piece == 'K':
+        moves += [(x-1, y-2), (x-1, y+2), (x-2, y+1), (x-2, y-1), (x+1, y-2), (x+1, y+2), (x+2, y-1), (x+2, y+1)]
+
+    elif boardPosition.boardArray[x][y].piece == 'B':
+        down_left = [(x-n, y-n) for n in range(1, 8)]
+        down_right = [(x+n, y-n) for n in range(1, 8)]
+        up_right = [(x+n, y+n) for n in range(1, 8)]
+        up_left = [(x-n, y+n) for n in range(1, 8)]
+        moves += down_left + down_right + up_right + up_left
+
+    print(moves)
+    return moves
+
 def drawBoard():
     count = 0
 
@@ -22,7 +51,7 @@ def drawBoard():
             if count % 2 == 0:
                 # print orange square
                 # rect(surface, color, rect), rect includes [int x, int y, width, height]
-                pygame.draw.rect(gameDisplay, GREEN, [squareSize*j + 300, squareSize*i + 200, squareSize, squareSize])
+                pygame.draw.rect(gameDisplay, WHITE, [squareSize*j + 300, squareSize*i + 200, squareSize, squareSize])
             else:
                 # print white squares
                 pygame.draw.rect(gameDisplay, ORANGE, [squareSize*j + 300, squareSize*i + 200, squareSize, squareSize])
@@ -33,7 +62,7 @@ def drawBoard():
 def getRectPoints(dest):
     # if the click is out of the bounds of the board
     if dest[0] < 300 or dest[0] > 940 or dest[1] < 200 or dest[1] > 840:
-        return (dest[0], dest[1])
+        return (dest[0], dest[1]) 
 
     # Coordinates of where the white and orange squares were drawn
     cornerPointsX = [860, 780, 700, 620, 540, 460, 380, 300]
@@ -42,19 +71,19 @@ def getRectPoints(dest):
     xPoint = dest[0]
     yPoint = dest[1]
 
-    # loop through each value in the list to find which
+    # loop through each value in the list to find which 
     # square the click was in
     for x in cornerPointsX:
         if xPoint > x:
             xPoint = x
             break
-
+    
     for y in cornerPointsY:
         if yPoint > y:
             yPoint = y
             break
 
-    return (xPoint, yPoint)
+    return (xPoint, yPoint) 
 
 # initialize pygame
 pygame.init()
@@ -69,10 +98,9 @@ gameDisplay = pygame.display.set_mode((1200, 1000))
 pygame.display.set_caption('Nickelodeon Chess')
 gameExit = False
 
-BLUE = (51, 153, 255)
+BLUE = (51, 153, 255) 
 WHITE = (255, 255, 255)
-ORANGE = (204,102,0)
-GREEN = (50, 205, 50)
+ORANGE = (204,102,0) 
 
 # blue background, we'll change as needed
 gameDisplay.fill(BLUE)
@@ -108,7 +136,7 @@ for k in range(0,8):
     gameDisplay.blit(TextSurf, TextRect)
 
     pygame.display.update()
-
+    
 #printBoardPieces(boardPosition)
 
 pygame.display.update()
@@ -134,7 +162,7 @@ print(spriteList)
 printBoardPieces(spriteList)
 
 # Will check if one of the sprites was selcted
-pieceClicked = False
+pieceClicked = False 
 
 # Will hold the image to redraw it on the board once the user
 # selected another square
@@ -150,9 +178,11 @@ while not gameExit:
 
             # Gets the x,y coordinates of the mouse
             pos = pygame.mouse.get_pos()
+            x_move, y_move = pygame.mouse.get_pos()
 
             # stores the sprite that user clicked
             clicked_sprite = [s for s in spriteList if s.rect.collidepoint(pos)]
+
 
             # will draw the image at the specified square
             if pieceClicked == True:
@@ -161,12 +191,11 @@ while not gameExit:
 
                 # Redraw board
                 drawBoard()
-
                 # get the x, y points for which square the sprite belongs in
                 pos = getRectPoints(pos)
 
                 # the the Object's rectangle data to the position
-                pieceSelected.rect = pygame.Rect(pos[0], pos[1], 80, 80)
+                pieceSelected.rect = pygame.Rect(pos[0], pos[1], 80, 80) 
 
                 # Redraw pieces
                 printBoardPieces(spriteList)
@@ -178,7 +207,8 @@ while not gameExit:
             if clicked_sprite:
                 pieceClicked = True
                 pieceSelected = clicked_sprite[0]
-
+                draw_move(clicked_sprite[0], spriteList)
+            
         if (event.type == pygame.QUIT):
             pygame.quit()
             quit()
